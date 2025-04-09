208952ce8d20b5e7570bc1885ad3d71d7d19addf
Deftu <me@deftu.dev>
2025-04-09 08:58:18 +0200

    Forgot Textualizer for options

--- localization-textualizer/src/main/kotlin/dev/deftu/akuma/textualizer/group.kt
+++ localization-textualizer/src/main/kotlin/dev/deftu/akuma/textualizer/group.kt
@@ -1,51 +1,9 @@
 package dev.deftu.akuma.textualizer
 
-import dev.deftu.akuma.CommandBuilder
 import dev.deftu.akuma.CommandGroupBuilder
 import dev.deftu.textualizer.Language
 import dev.deftu.textualizer.Localization
 import net.dv8tion.jda.api.interactions.DiscordLocale
-
-public fun <T : Language<T>> CommandBuilder.applyLocalizedNameTranslations(
-    localization: Localization<T>,
-    languages: Collection<T>,
-    converter: (T) -> DiscordLocale,
-    key: String
-) {
-    nameLocalizations.clear()
-
-    for (language in languages) {
-        val locale = converter(language)
-        val context = localization.with(language)
-        nameLocalizations[locale] = context.get(key)
-    }
-}
-
-public fun <T : Language<T>> CommandBuilder.applyLocalizedDescriptionTranslations(
-    localization: Localization<T>,
-    languages: Collection<T>,
-    converter: (T) -> DiscordLocale,
-    key: String
-) {
-    descriptionLocalizations.clear()
-
-    for (language in languages) {
-        val locale = converter(language)
-        val context = localization.with(language)
-        descriptionLocalizations[locale] = context.get(key)
-    }
-}
-
-public fun <T : Language<T>> CommandBuilder.applyLocalizedTranslations(
-    localization: Localization<T>,
-    languages: Collection<T>,
-    converter: (T) -> DiscordLocale,
-    nameKey: String,
-    descriptionKey: String
-) {
-    applyLocalizedNameTranslations(localization, languages, converter, nameKey)
-    applyLocalizedDescriptionTranslations(localization, languages, converter, descriptionKey)
-}
 
 public fun <T : Language<T>> CommandGroupBuilder.applyLocalizedNameTranslations(
     localization: Localization<T>,
---
+++ localization-textualizer/src/main/kotlin/dev/deftu/akuma/textualizer/option.kt
@@ -0,0 +1,47 @@
+package dev.deftu.akuma.textualizer
+
+import dev.deftu.akuma.OptionBuilder
+import dev.deftu.textualizer.Language
+import dev.deftu.textualizer.Localization
+import net.dv8tion.jda.api.interactions.DiscordLocale
+
+public fun <T : Language<T>> OptionBuilder.applyLocalizedNameTranslations(
+    localization: Localization<T>,
+    languages: Collection<T>,
+    converter: (T) -> DiscordLocale,
+    key: String
+) {
+    nameLocalizations.clear()
+
+    for (language in languages) {
+        val locale = converter(language)
+        val context = localization.with(language)
+        nameLocalizations[locale] = context.get(key)
+    }
+}
+
+public fun <T : Language<T>> OptionBuilder.applyLocalizedDescriptionTranslations(
+    localization: Localization<T>,
+    languages: Collection<T>,
+    converter: (T) -> DiscordLocale,
+    key: String
+) {
+    descriptionLocalizations.clear()
+
+    for (language in languages) {
+        val locale = converter(language)
+        val context = localization.with(language)
+        descriptionLocalizations[locale] = context.get(key)
+    }
+}
+
+public fun <T : Language<T>> OptionBuilder.applyLocalizedTranslations(
+    localization: Localization<T>,
+    languages: Collection<T>,
+    converter: (T) -> DiscordLocale,
+    nameKey: String,
+    descriptionKey: String
+) {
+    applyLocalizedNameTranslations(localization, languages, converter, nameKey)
+    applyLocalizedDescriptionTranslations(localization, languages, converter, descriptionKey)
+}
